5624c6964089f1e2cfc912a5c9fa00600377ae5f
dlsh <dlsh3858@gmail.com>
2021-03-12 19:05:20 +0800

    handle different image file types

--- background.js
+++ background.js
@@ -11,44 +11,69 @@ function main(currTab) {
 }
 
 
-function dataURLtoBlob(dataurl) {
-    // Split entire data url into 2
-    let arr = dataurl.split(',');
-
-    // matches for portion in-between : and ;
-    let mime = arr[0].match(/:(.*?);/)[1];
-    
-    // decode base-64
-    let binarystr = atob(arr[1]);
-    let n = binarystr.length;
-
-    // recode as utf-8
-    let u8arr = new Uint8Array(n);
-
-    while (n--) {
-        u8arr[n] = binarystr.charCodeAt(n);
-    }
-    return new Blob([u8arr], {type:mime});
-}
-
 /*
     Downloads favicon of current tab
 */ 
 function downloadIcon(dataUrl) {
 
     let dataBlob = dataURLtoBlob(dataUrl);
+    console.log(dataUrl);
 
     const downloadUrl = URL.createObjectURL(dataBlob);
 
+    const dataType = findFileType(dataUrl);
+
     const options = {
         url: downloadUrl,
-        filename: 'favicon.ico',
+        filename: 'favicon' + dataType,
         saveAs: true
     }
 
     let downloading = browser.downloads.download(options);
 
     downloading.then((id) => downloadStartSuccess(id, downloadUrl), downloadFail);
+}
+
+/*
+    Helper functions
+*/
+
+function dataURLtoBlob(dataUrl) {
+    // Split entire data url into 2
+    let arr = dataUrl.split(',');
+
+    // matches for portion in-between : and ;
+    let mime = arr[0].match(/:(.*?);/)[1];
+
+    // decode base-64
+    let binaryStr = atob(arr[1]);
+    let n = binaryStr.length;
+
+    // recode as utf-8
+    let u8Arr = new Uint8Array(n);
+
+    while (n--) {
+        u8Arr[n] = binaryStr.charCodeAt(n);
+    }
+    return new Blob([u8Arr], {type:mime});
+}
+
+function findFileType(dataUrl) {
+    // Split entire data url into 2
+    let arr = dataUrl.split(',');
+
+    // match for image type only
+    let imageType = arr[0].match(/\/(.*?);/)[1];
+
+    if (imageType == "x-icon") {
+        return ".ico";
+    } else if (imageType == "svg+xml") {
+        return ".svg";
+    } else {
+        return ".png";
+    }
+    
+      
 }
 
 /*
